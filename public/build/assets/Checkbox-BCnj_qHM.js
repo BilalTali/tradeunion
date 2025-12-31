cfac498728c427f8cb179d84b0b1ec26a1155969
TASLEEMAH <tasleemah@example.com>
2025-12-31 22:08:54 +0530

    chore: Push production build assets (Deployment)

--- public/build/assets/Checkbox-BCnj_qHM.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-DvqN5tN9.js";function t({className:r="",...e}){return o.jsx("input",{...e,type:"checkbox",className:"rounded border-gray-300 text-red-600 shadow-sm focus:ring-red-500 "+r})}export{t as C};
